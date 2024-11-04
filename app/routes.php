bfb6e2c877ce9e7969774294615d6c457ac2cb37
Vinicius Silva <viniciusbarbosa@prefeitura.sp.gov.br>
2024-11-04 15:24:48 -0300

    atualizando grupo de rotas

--- app/routes.php
+++ app/routes.php
@@ -49,104 +49,102 @@ return function (App $app) {
             return $response;
         });
     });
-    $app->group('/caroldance', function (Group $group) {
 
-        $group->group('/teste', function (Group $teste) {
-            $teste->get('', Teste::class);
+    $app->group('/teste', function (Group $teste) {
+        $teste->get('', Teste::class);
 
-            $teste->post('/csrf', function (Request $request, Response $response) {
-                $response->getBody()->write('Deu certo');
-                return $response;
-            });
-            $teste->get('/jwt', function (Request $request, Response $response) {
-                $response->getBody()->write('Deu certo');
-                return $response;
-            })->add(AuthenticateUserMiddleware::class);
-            $teste->post('/csrfjwt', function (Request $request, Response $response) {
-                $response->getBody()->write('Deu certo');
-                return $response;
-            })->add(AuthenticateUserMiddleware::class);
+        $teste->post('/csrf', function (Request $request, Response $response) {
+            $response->getBody()->write('Deu certo');
+            return $response;
         });
-
-        $group->group('/signin', function (Group $group) {
-            $group->post('', SigninLoginAction::class);
-            $group->post('/register', UserLoginRegisterAction::class);
-            $group->post('/linkForgotPassword', SigninLinkForgotPasswordAction::class);
-            $group->post('/changePassword', SigninChangePasswordAction::class);
-        });
-
-        $group->group('/user', function (Group $user) {
-            $user->post('/{id}', UserViewAction::class);
+        $teste->get('/jwt', function (Request $request, Response $response) {
+            $response->getBody()->write('Deu certo');
+            return $response;
         })->add(AuthenticateUserMiddleware::class);
+        $teste->post('/csrfjwt', function (Request $request, Response $response) {
+            $response->getBody()->write('Deu certo');
+            return $response;
+        })->add(AuthenticateUserMiddleware::class);
+    });
 
-        $group->group('/clientTicket', function(Group $clientTicket) {
-            $clientTicket->group('/ticket', function(Group $ticket) {
-                $ticket->post('/buy', ClientTicketPurchaseAction::class);
-                $ticket->put('/cancel', ClientTicketCancelPurchaseAction::class);
-                $ticket->put('/confirm', ClientTicketConfirmPurchaseAction::class);
-                $ticket->get('', ClientTicketListAction::class);
-                $ticket->post('/validateTicket', ClientTicketValidateTicketAction::class);
-                $ticket->post('/enableValidateTicket', ClientTicketEnableValidateTicketAction::class);
-            });
-            $clientTicket->group('/seat', function(Group $seat) {
-                $seat->get('', ClientTicketListSeatsAction::class);
-            });
-            $clientTicket->group('/parking', function(Group $ticket) {
-                $ticket->post('/buy', ClientTicketPurchaseParkingAction::class);
-                $ticket->put('/confirm', ClientTicketConfirmPurchaseParkingAction::class);
-                $ticket->put('/cancel', ClientTicketCancelPurchaseParkingAction::class);
-                $ticket->get('', ClientTicketListParkingAction::class);
-            });
-            
+    $app->group('/signin', function (Group $group) {
+        $group->post('', SigninLoginAction::class);
+        $group->post('/register', UserLoginRegisterAction::class);
+        $group->post('/linkForgotPassword', SigninLinkForgotPasswordAction::class);
+        $group->post('/changePassword', SigninChangePasswordAction::class);
+    });
+
+    $app->group('/user', function (Group $user) {
+        $user->post('/{id}', UserViewAction::class);
+    })->add(AuthenticateUserMiddleware::class);
+
+    $app->group('/clientTicket', function(Group $clientTicket) {
+        $clientTicket->group('/ticket', function(Group $ticket) {
+            $ticket->post('/buy', ClientTicketPurchaseAction::class);
+            $ticket->put('/cancel', ClientTicketCancelPurchaseAction::class);
+            $ticket->put('/confirm', ClientTicketConfirmPurchaseAction::class);
+            $ticket->get('', ClientTicketListAction::class);
+            $ticket->post('/validateTicket', ClientTicketValidateTicketAction::class);
+            $ticket->post('/enableValidateTicket', ClientTicketEnableValidateTicketAction::class);
         });
-
-        $group->group('/admin', function (Group $group) {
-            $group->group('/student', function (Group $student) {
-                $student->post('/register', StudentRegisterAction::class);
-                $student->get('/list', StudentListAction::class);
-                $student->get('/export/{extension}', StudentExportAction::class);
-            });
-            $group->group('/user', function (Group $user) {
-                $user->post('/register', UserAdminRegisterAction::class);
-                $user->get('/list', UserListAction::class);
-                $user->get('/export/{extension}', UserExportAction::class);
-
-            });
-            $group->group('/activityStudent', function (Group $activityStudent) {
-                $activityStudent->post('/register', ActivityStudentRegisterAction::class);
-                $activityStudent->get('/list', ActivityStudentListAction::class);
-            });
-            $group->group('/monthlyPayment', function (Group $monthlyPayment) {
-                // $monthlyPayment->post('/register', ActivityStudentRegisterAction::class);
-                $monthlyPayment->get('/list', MonthlyPaymentListAction::class);
-            });
+        $clientTicket->group('/seat', function(Group $seat) {
+            $seat->get('', ClientTicketListSeatsAction::class);
         });
-        //->add(AuthenticateUserMiddleware::class);
-
-        $group->group('/token', function (Group $token) {
-            $token->get('/csrf', function(Request $request, Response $response){
-                $token = (new GenerateTokenCSRFMiddleware($this->get('database')))->getToken();
-                $response->getBody()->write(json_encode(["statusCode" => 200], JSON_PRETTY_PRINT));
-                return $response->withHeader('Content-Type', 'application/json')->withHeader('Set-Cookie', "X-Csrf-Token=$token; Path=/; HttpOnly; Secure; SameSite=None");
-            });
-            $token->get('/recaptcha/{token}', RecaptchaMiddleware::class);
-            $token->get('/forgotPassword/{token}', ValidateTokenJWTMiddleware::class);
-            $token->get('/validateTokenJWT/{token}', ValidateTokenJWTMiddleware::class);
+        $clientTicket->group('/parking', function(Group $ticket) {
+            $ticket->post('/buy', ClientTicketPurchaseParkingAction::class);
+            $ticket->put('/confirm', ClientTicketConfirmPurchaseParkingAction::class);
+            $ticket->put('/cancel', ClientTicketCancelPurchaseParkingAction::class);
+            $ticket->get('', ClientTicketListParkingAction::class);
         });
+        
+    });
 
-        $group->group('/services', function (Group $services) {
-            $services->group('/facebook', function (Group $facebook) {
-                $facebook->get('/oAuth/{token}', FacebookOAuth::class);
-            });
-            $services->group('/google', function (Group $google) {
-                $google->get('/oAuth/{token}', GoogleOAuth::class);
-            });
-            $services->group('/mercadopago', function (Group $mercadopago) {
-                $mercadopago->get('/createPreference', MercadoPagoCreatePreference::class);
-                $mercadopago->get('/callback', MercadoPagoCallback::class);
-                $mercadopago->get('/notification', MercadoPagoNotification::class);
-                $mercadopago->get('/pagamento', MercadoPagoPagamento::class);
-            });
+    $app->group('/admin', function (Group $group) {
+        $group->group('/student', function (Group $student) {
+            $student->post('/register', StudentRegisterAction::class);
+            $student->get('/list', StudentListAction::class);
+            $student->get('/export/{extension}', StudentExportAction::class);
+        });
+        $group->group('/user', function (Group $user) {
+            $user->post('/register', UserAdminRegisterAction::class);
+            $user->get('/list', UserListAction::class);
+            $user->get('/export/{extension}', UserExportAction::class);
+
+        });
+        $group->group('/activityStudent', function (Group $activityStudent) {
+            $activityStudent->post('/register', ActivityStudentRegisterAction::class);
+            $activityStudent->get('/list', ActivityStudentListAction::class);
+        });
+        $group->group('/monthlyPayment', function (Group $monthlyPayment) {
+            // $monthlyPayment->post('/register', ActivityStudentRegisterAction::class);
+            $monthlyPayment->get('/list', MonthlyPaymentListAction::class);
+        });
+    });
+    //->add(AuthenticateUserMiddleware::class);
+
+    $app->group('/token', function (Group $token) {
+        $token->get('/csrf', function(Request $request, Response $response){
+            $token = (new GenerateTokenCSRFMiddleware($this->get('database')))->getToken();
+            $response->getBody()->write(json_encode(["statusCode" => 200], JSON_PRETTY_PRINT));
+            return $response->withHeader('Content-Type', 'application/json')->withHeader('Set-Cookie', "X-Csrf-Token=$token; Path=/; HttpOnly; Secure; SameSite=None");
+        });
+        $token->get('/recaptcha/{token}', RecaptchaMiddleware::class);
+        $token->get('/forgotPassword/{token}', ValidateTokenJWTMiddleware::class);
+        $token->get('/validateTokenJWT/{token}', ValidateTokenJWTMiddleware::class);
+    });
+
+    $app->group('/services', function (Group $services) {
+        $services->group('/facebook', function (Group $facebook) {
+            $facebook->get('/oAuth/{token}', FacebookOAuth::class);
+        });
+        $services->group('/google', function (Group $google) {
+            $google->get('/oAuth/{token}', GoogleOAuth::class);
+        });
+        $services->group('/mercadopago', function (Group $mercadopago) {
+            $mercadopago->get('/createPreference', MercadoPagoCreatePreference::class);
+            $mercadopago->get('/callback', MercadoPagoCallback::class);
+            $mercadopago->get('/notification', MercadoPagoNotification::class);
+            $mercadopago->get('/pagamento', MercadoPagoPagamento::class);
         });
     });
 };
