7ea1315dc940f69014452c103e4a641e1ec295be
Vinicius Silva <viniciusbarbosa@prefeitura.sp.gov.br>
2024-11-15 20:02:32 -0300

    alterado valor do ingresso

--- src/Application/Actions/ClientTicket/ClientTicketListSeatsAction.php
+++ src/Application/Actions/ClientTicket/ClientTicketListSeatsAction.php
@@ -82,7 +82,7 @@ class ClientTicketListSeatsAction extends ClientTicketAction {
                 if(!in_array($letter, $freeSeats))
                     $valor = 40;
             }
-            $data[$k] = ["id" => $v->getId(), "name" => $v->getAssento(), "disponivel" => $isAvaible, "valor" => 40];
+            $data[$k] = ["id" => $v->getId(), "name" => $v->getAssento(), "disponivel" => $isAvaible, "valor" => $valor];
 
         }
     }
