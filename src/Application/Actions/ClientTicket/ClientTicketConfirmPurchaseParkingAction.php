8eb331289499fb8c5d96aee0cfcf6703f84c0104
Vinicius Silva <viniciusbarbosa@prefeitura.sp.gov.br>
2024-11-23 16:11:33 -0300

    adicionado qrcode na confirmação do estacionamento

--- src/Application/Actions/ClientTicket/ClientTicketConfirmPurchaseParkingAction.php
+++ src/Application/Actions/ClientTicket/ClientTicketConfirmPurchaseParkingAction.php
@@ -6,6 +6,14 @@ namespace App\Application\Actions\ClientTicket;
 use App\Domain\DomainException\CustomDomainException;
 use App\Domain\ParkingTicket\ParkingTicket;
 use Psr\Http\Message\ResponseInterface as Response;
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Color\Color;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\Label\Font\OpenSans;
+use Endroid\QrCode\Label\LabelAlignment;
+use Endroid\QrCode\RoundBlockSizeMode;
+use Endroid\QrCode\Writer\PngWriter;
 
 class ClientTicketConfirmPurchaseParkingAction extends ClientTicketAction {
 
@@ -29,8 +37,16 @@ class ClientTicketConfirmPurchaseParkingAction extends ClientTicketAction {
         Valor Total: R$15 <br>
         Valido para: " . $client->getPeriodo();
 
-
-        $this->sendMail("Carol Dance - O verdadeiro presente de natal!", $bodyMail, [$client->getEmail()], [], ['vini15_silva@hotmail.com']);
+        $payload = [
+            "nome" => $client->getNome(),
+            "cpf"  => $client->getCpf(),
+            "seatName" => "",
+            "periodo"  => $client->getPeriodo(),
+            "nomeIngresso" => "Estacionamento - " . $client->getPeriodo()
+        ];
+        $qrcodes[] = self::generateQRCODE($payload, 12);
+        // $this->sendMail("Carol Dance - O verdadeiro presente de natal!", $bodyMail, [$client->getEmail()], [], ['vini15_silva@hotmail.com']);
+        $this->sendMail("Carol Dance - O verdadeiro presente de natal!", $bodyMail, [$client->getEmail()], [], ['vini15_silva@hotmail.com', 'biabarros10@icloud.com'], false, [], false, '', true, $qrcodes);
         $this->database->commit();
 
         return $this->respondWithData();
@@ -45,5 +61,27 @@ class ClientTicketConfirmPurchaseParkingAction extends ClientTicketAction {
             throw new CustomDomainException('O pagamento já foi concluido!');
         
         return $parking;
+    }
+    private function generateQRCODE($data, $lengthTextQRCode=13){
+        $baseUrl = "https://carol-dance-web.netlify.app/qrcode";
+        $url = $baseUrl . "?NOME=".urlencode($data['nome'])."&CPF=".urlencode($data['cpf'])."&ASSENTOS=".urlencode($data['seatName'])."&SESSSAO=".urlencode($data['periodo']);
+        $builder = new Builder(
+            new PngWriter(),
+            [],
+            false,
+            $url,
+            new Encoding('UTF-8'),
+            ErrorCorrectionLevel::High,
+            300,
+            10,
+            RoundBlockSizeMode::Margin,
+            new Color(0,0,0),
+            new Color(255,255,255),
+            $data["nomeIngresso"],
+            new OpenSans($lengthTextQRCode),
+            LabelAlignment::Center 
+        );
+        $result = $builder->build();    
+        return ["data" => $result->getString(), "name" => $data['periodo'] . '.png', "typeMIME" => 'base64', "typeImage" => "image/png"];
     }
 }
